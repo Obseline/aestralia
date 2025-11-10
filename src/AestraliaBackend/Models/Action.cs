976c9b386105626639835bcf1ba469f332984e42
Julien THOMAS <thomas.julien@protonmail.com>
2025-11-10 14:16:22 +0100

    Action: Add missing doc comments

--- src/AestraliaBackend/Models/Action.cs
+++ src/AestraliaBackend/Models/Action.cs
@@ -1,34 +1,38 @@
 namespace AestraliaBackend.Models
 {
     /// <summary>
-    /// Class <c>Action</c> models TODO
+    /// Class <c>Action</c> models an Action a <see>Villager</see> can perform.
     /// </summary>
     public class Action(string title)
     {
         /// <summary>
-        /// TODO
+        /// Unique identifier.
         /// </summary>
         public int id;
 
         /// <summary>
-        /// TODO
+        /// The name of the Action.
         /// </summary>
         public string title = title;
 
         /// <summary>
-        /// TODO
+        /// The "cost" of this Action, see <see>NeedFactor</see>. 
         /// </summary>
-        public int needs_factors;
+        public NeedFactor needs_factors;
 
         /// <summary>
-        /// TODO
+        /// Some actions consume/produce <see>Item</see>s.
         /// </summary>
+        /// A negative value for the "amount" of an <see>Item</see> means it
+        /// will comsume it.
+        /// A positive value for the "amount" of an <see>Item</see> means it
+        /// will produce it.
         public Item[] items = [];
 
         /// <summary>
-        /// TODO
-        /// </summary>
+        /// The proficiency a <see>Villager</see> will gain when perform this Action.
         /// NOTE: winux Should we rename that field to something like "xp_reward" ?
+        /// </summary>
         public int gain_experience;
 
     }
