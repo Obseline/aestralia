cb508bff42b285f68b25559f505568b964f1ca1b
Julien THOMAS <thomas.julien@protonmail.com>
2026-01-08 14:25:21 +0100

    Program: Draw a map

--- src/AestraliaBackend/Program.cs
+++ src/AestraliaBackend/Program.cs
@@ -1,3 +1,4 @@
+using AestraliaBackend.Models;
 using AestraliaBackend.Network;
 using Microsoft.Extensions.Logging;
 
@@ -9,6 +10,11 @@ class Program
 
         var server = new WebSocketServer("http://localhost:34000/");
         var t = Task.Run(server.Listen);
+
+        var map = new Map("Aestralia", 100, 100);
+        map.ToPpm("aestralia01");
+        map.SpawnVillages(15);
+        map.ToPpm("aestralia02");
 
         // NOTE: winux Infinite loop, `t` never returns
         await t;
